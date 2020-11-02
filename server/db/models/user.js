37e95e5f54a937124ba90f0e6ae0ee4a86df3be7
Chris Miller <chris@paro.io>
2020-11-01 18:08:55 -0600

    google auth and graphql

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -2,10 +2,21 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const User = db.define('user', {
-  name: {
+  email: {
     type: Sequelize.STRING,
     unique: true,
+    allowNull: true
   },
+  name: {
+    type: Sequelize.STRING,
+    unique: false,
+    allowNull: true
+  },
+  googleId: {
+    type: Sequelize.STRING,
+    unique: false,
+    allowNull: true
+  }
 })
 
 module.exports = User
